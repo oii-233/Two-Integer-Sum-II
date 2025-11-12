dc3a5a988c0883f16662758f59e00c956481839f
Iman Ibrahim <imanibbb544@gmail.com>
2025-11-12 19:22:23 +0300

    Add twoSum function to solve two-sum problem
    Implement twoSum function to find indices of two numbers that add up to the target.

---
+++ .py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        length=len(numbers)
+        left=0
+        right=length-1
+        adi=[]
+        while left < right:
+            total=numbers[left]+numbers[right]
+            if total==target:
+                adi.append(left+1)
+                adi.append(right+1)
+                break
+            elif total>target:
+                right-=1
+            else:
+                left+=1
+        return adi
